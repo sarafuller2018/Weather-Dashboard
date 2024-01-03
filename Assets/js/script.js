f70e71f618dfd1a2a381cb48e3c0e09b7d8687e5
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-01-02 19:52:25 -0600

    created README, fixed styling, and date format

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,12 +9,12 @@ var card2El = document.getElementById("card2");
 var card3El = document.getElementById("card3");
 var card4El = document.getElementById("card4");
 var card5El = document.getElementById("card5");
-
 var savedCitiesArray = [];
+
+// pulls local storage if it exists, if not uses an empty array
 console.log(JSON.parse(localStorage.getItem("cities")));
 
 if (JSON.parse(localStorage.getItem("cities")) !== null) {
-    console.log("there is an array");
     savedCitiesArray = JSON.parse(localStorage.getItem("cities"));
 };
 
@@ -46,7 +46,7 @@ function getGeoCoding(city) {
         });
 };
 
-//retreives current weather for city and adds to page
+// retreives current weather for city and adds to page
 function getCurrentWeather(lat, long) {
 
     currentWeatherDivEl.innerHTML = "";
@@ -84,11 +84,10 @@ function getCurrentWeather(lat, long) {
         });
 };
 
-//retrieves the 5-day weather for city and adds to page
+// retrieves the 5-day weather for city and adds to page
 function get5DayWeather(lat, long) {
 
     fiveDayWeatherEl.innerHTML = "";
-
 
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&appid=" + APIKey + "&units=imperial";
     fetch(queryURL)
@@ -101,159 +100,42 @@ function get5DayWeather(lat, long) {
             var create5DayHeader = document.createElement("h2");
             create5DayHeader.textContent = "5-Day Forecast";
             fiveDayWeatherEl.appendChild(create5DayHeader);
-            var forecastSection = document.createElement("div")
-            forecastSection.classList.add("fiveDaySection")
-            
-          
-            for(var i = 2; i < 40; i+=8){
+
+            var forecastSection = document.createElement("div");
+            forecastSection.classList.add("fiveDaySection");
+            forecastSection.setAttribute("class", "fiveDayWrap");
+
+            // iterates through 5 day forecast data and appends to page
+            for (var i = 2; i < 40; i += 8) {
                 console.log(i)
-                var createCard1 = document.createElement("div");
-                var Date1 = document.createElement("p");
-                var Temp1 = document.createElement("p");
-                var Wind1 = document.createElement("p");
-                var Humidity1 = document.createElement("p");
-                var weatherIcon1 = data.list[i].weather[0].icon;
-                var loadWeatherIcon1 = document.createElement("img");
-                loadWeatherIcon1.setAttribute("src", "http://openweathermap.org/img/w/" + weatherIcon1 + ".png");
-    
-                Date1.textContent = data.list[i].dt_txt;
-                Date1.classList.add("bold");
-                Temp1.textContent = "Temperature: " + data.list[i].main.temp + " °F";
-                Wind1.textContent = "Wind: " + data.list[i].wind.speed + " MPH";
-                Humidity1.textContent = "Humidity: " + data.list[i].main.humidity + "%";
-    
-                createCard1.appendChild(loadWeatherIcon1)
-                createCard1.appendChild(Date1)
-                createCard1.appendChild(Temp1)
-                createCard1.appendChild(Wind1)
-                createCard1.appendChild(Humidity1)
-                forecastSection.appendChild(createCard1);
-                fiveDayWeatherEl.appendChild(forecastSection)
-            }
-            // Day 1 weather info
-            // var createCard1 = document.createElement("div");
-            // var Date1 = document.createElement("p");
-            // var Temp1 = document.createElement("p");
-            // var Wind1 = document.createElement("p");
-            // var Humidity1 = document.createElement("p");
-            // var weatherIcon1 = data.list[2].weather[0].icon;
-            // var loadWeatherIcon1 = document.createElement("img");
-            // loadWeatherIcon1.setAttribute("src", "http://openweathermap.org/img/w/" + weatherIcon1 + ".png");
+                var createCard = document.createElement("div");
+                createCard.setAttribute("class", "paddingNoWrap")
+                var Date = document.createElement("p");
+                var Temp = document.createElement("p");
+                var Wind = document.createElement("p");
+                var Humidity = document.createElement("p");
+                var weatherIcon = data.list[i].weather[0].icon;
+                var loadWeatherIcon = document.createElement("img");
+                loadWeatherIcon.setAttribute("src", "http://openweathermap.org/img/w/" + weatherIcon + ".png");
 
-            // Date1.textContent = data.list[2].dt_txt;
-            // Date1.classList.add("bold");
-            // Temp1.textContent = "Temperature: " + data.list[2].main.temp + " °F";
-            // Wind1.textContent = "Wind: " + data.list[2].wind.speed + " MPH";
-            // Humidity1.textContent = "Humidity: " + data.list[2].main.humidity + "%";
+                Date.textContent = dayjs(data.list[i].dt_txt).format("MM/DD/YYYY");
+                Date.classList.add("bold");
+                Temp.textContent = "Temperature: " + data.list[i].main.temp + " °F";
+                Wind.textContent = "Wind: " + data.list[i].wind.speed + " MPH";
+                Humidity.textContent = "Humidity: " + data.list[i].main.humidity + "%";
 
-            // createCard1.appendChild(loadWeatherIcon1)
-            // createCard1.appendChild(Date1)
-            // createCard1.appendChild(Temp1)
-            // createCard1.appendChild(Wind1)
-            // createCard1.appendChild(Humidity1)
-            // fiveDayWeatherEl.appendChild(createCard1);
-
-            // fiveDayWeatherEl.children[1].appendChild(loadWeatherIcon1);
-            // fiveDayWeatherEl.children[1].appendChild(Date1);
-            // fiveDayWeatherEl.children[1].appendChild(Temp1);
-            // fiveDayWeatherEl.children[1].appendChild(Wind1);
-            // fiveDayWeatherEl.children[1].appendChild(Humidity1);
-
-            // Day 2 weather info
-            // var createCard2 = document.createElement("div");
-            // var Date2 = document.createElement("p");
-            // var Temp2 = document.createElement("p");
-            // var Wind2 = document.createElement("p");
-            // var Humidity2 = document.createElement("p");
-            // var weatherIcon2 = data.list[10].weather[0].icon;
-            // var loadWeatherIcon2 = document.createElement("img");
-            // loadWeatherIcon2.setAttribute("src", "http://openweathermap.org/img/w/" + weatherIcon2 + ".png");
-
-            // Date2.textContent = data.list[10].dt_txt;
-            // Date2.classList.add("bold");
-            // Temp2.textContent = "Temperature: " + data.list[10].main.temp + " °F";
-            // Wind2.textContent = "Wind: " + data.list[10].wind.speed + " MPH";
-            // Humidity2.textContent = "Humidity: " + data.list[10].main.humidity + "%";
-
-            // fiveDayWeatherEl.appendChild(createCard2);
-            // fiveDayWeatherEl.children[2].appendChild(loadWeatherIcon2);
-            // fiveDayWeatherEl.children[2].appendChild(Date2);
-            // fiveDayWeatherEl.children[2].appendChild(Temp2);
-            // fiveDayWeatherEl.children[2].appendChild(Wind2);
-            // fiveDayWeatherEl.children[2].appendChild(Humidity2);
-
-            // Day 3 weather info
-            // var createCard3 = document.createElement("div");
-            // var Date3 = document.createElement("p");
-            // var Temp3 = document.createElement("p");
-            // var Wind3 = document.createElement("p");
-            // var Humidity3 = document.createElement("p");
-            // var weatherIcon3 = data.list[18].weather[0].icon;
-            // var loadWeatherIcon3 = document.createElement("img");
-            // loadWeatherIcon3.setAttribute("src", "http://openweathermap.org/img/w/" + weatherIcon3 + ".png");
-
-            // Date3.textContent = data.list[18].dt_txt;
-            // Date3.classList.add("bold");
-            // Temp3.textContent = "Temperature: " + data.list[18].main.temp + " °F";
-            // Wind3.textContent = "Wind: " + data.list[18].wind.speed + " MPH";
-            // Humidity3.textContent = "Humidity: " + data.list[18].main.humidity + "%";
-
-            // fiveDayWeatherEl.appendChild(createCard3);
-            // fiveDayWeatherEl.children[3].appendChild(loadWeatherIcon3);
-            // fiveDayWeatherEl.children[3].appendChild(Date3);
-            // fiveDayWeatherEl.children[3].appendChild(Temp3);
-            // fiveDayWeatherEl.children[3].appendChild(Wind3);
-            // fiveDayWeatherEl.children[3].appendChild(Humidity3);
-
-            // Day 4 weather info
-            // var createCard4 = document.createElement("div");
-            // var Date4 = document.createElement("p");
-            // var Temp4 = document.createElement("p");
-            // var Wind4 = document.createElement("p");
-            // var Humidity4 = document.createElement("p");
-            // var weatherIcon4 = data.list[26].weather[0].icon;
-            // var loadWeatherIcon4 = document.createElement("img");
-            // loadWeatherIcon4.setAttribute("src", "http://openweathermap.org/img/w/" + weatherIcon4 + ".png");
-
-            // Date4.textContent = data.list[26].dt_txt;
-            // Date4.classList.add("bold");
-            // Temp4.textContent = "Temperature: " + data.list[26].main.temp + " °F";
-            // Wind4.textContent = "Wind: " + data.list[26].wind.speed + " MPH";
-            // Humidity4.textContent = "Humidity: " + data.list[26].main.humidity + "%";
-
-            // fiveDayWeatherEl.appendChild(createCard4);
-            // fiveDayWeatherEl.children[4].appendChild(loadWeatherIcon4);
-            // fiveDayWeatherEl.children[4].appendChild(Date4);
-            // fiveDayWeatherEl.children[4].appendChild(Temp4);
-            // fiveDayWeatherEl.children[4].appendChild(Wind4);
-            // fiveDayWeatherEl.children[4].appendChild(Humidity4);
-
-            // Day 5 weather info
-            // var createCard5 = document.createElement("div");
-            // var Date5 = document.createElement("p");
-            // var Temp5 = document.createElement("p");
-            // var Wind5 = document.createElement("p");
-            // var Humidity5 = document.createElement("p");
-            // var weatherIcon5 = data.list[34].weather[0].icon;
-            // var loadWeatherIcon5 = document.createElement("img");
-            // loadWeatherIcon5.setAttribute("src", "http://openweathermap.org/img/w/" + weatherIcon5 + ".png");
-
-            // Date5.textContent = data.list[34].dt_txt;
-            // Date5.classList.add("bold");
-            // Temp5.textContent = "Temperature: " + data.list[34].main.temp + " °F";
-            // Wind5.textContent = "Wind: " + data.list[34].wind.speed + " MPH";
-            // Humidity5.textContent = "Humidity: " + data.list[34].main.humidity + "%";
-
-            // fiveDayWeatherEl.appendChild(createCard5);
-            // fiveDayWeatherEl.children[5].appendChild(loadWeatherIcon5);
-            // fiveDayWeatherEl.children[5].appendChild(Date5);
-            // fiveDayWeatherEl.children[5].appendChild(Temp5);
-            // fiveDayWeatherEl.children[5].appendChild(Wind5);
-            // fiveDayWeatherEl.children[5].appendChild(Humidity5);
+                createCard.appendChild(loadWeatherIcon);
+                createCard.appendChild(Date);
+                createCard.appendChild(Temp);
+                createCard.appendChild(Wind);
+                createCard.appendChild(Humidity);
+                forecastSection.appendChild(createCard);
+                fiveDayWeatherEl.appendChild(forecastSection);
+            };
         });
 };
 
-//adds button with city to page
+// adds button with city to page
 function saveCityButton(city) {
     var addHistory = document.createElement("button");
     addHistory.setAttribute("class", "w-75 m-2");
@@ -271,39 +153,29 @@ function saveCityButton(city) {
     localStorage.setItem("cities", JSON.stringify(savedCitiesArray));
 };
 
+// allows page to pull search history and add to page
 function renderHistory() {
-   var cityArray = JSON.parse(localStorage.getItem("cities"));
-   console.log(cityArray);
+    var cityArray = JSON.parse(localStorage.getItem("cities"));
+    console.log(cityArray);
 
-   for (var i=0; i<cityArray.length; i++) {
-    console.log(cityArray[i]);
-    var addHistory = document.createElement("button");
-    addHistory.setAttribute("class", "w-75 m-2");
-    addHistory.textContent = cityArray[i];
-    addHistory.addEventListener("click", function (event) {
+    for (var i = 0; i < cityArray.length; i++) {
+        console.log(cityArray[i]);
+        var addHistory = document.createElement("button");
+        addHistory.setAttribute("class", "w-75 m-2");
+        addHistory.textContent = cityArray[i];
+        addHistory.addEventListener("click", function (event) {
 
-        var city = event.target.textContent;
-        getGeoCoding(city)
-    });
+            var city = event.target.textContent;
+            getGeoCoding(city)
+        });
 
-    searchHistoryEl.appendChild(addHistory);
-   }
+        searchHistoryEl.appendChild(addHistory);
+    }
+};
 
-}
-
+//calls search history on init of page 
 function init() {
     renderHistory();
-}
+};
 
 init();
-
-// function addTwoNumbers(numOne, numTwo){
-
-//console.log(numOne + numTwo)
-// }
-// addTwoNumbers(10, 6);
-// addTwoNumbers(5,9);
-
-//local storage
-//styling
-//consolidate
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Weather-Dashboard
+
+
+
+## Description
+
+This is a website designed to pull current and future weather conditions for a given city.
+
+## Installation
+
+N/A
+
+## Usage
+
+When the user submits a city, they will be presented with both current and future weather conditions including the date, an icon, the temperature, the wind, and the humidity. Their input will be saved to local storage for future use, and they can click on any button in the search history to revisit a city's weather.
+
+## Credits
+
+Received help with pieces of this code from: 
+Juan Delgado - tutor
+
+## License
+
+Please refer to the LICENSE in the repo.
+
+## Screenshot of Website
+
+![Alt text]()
